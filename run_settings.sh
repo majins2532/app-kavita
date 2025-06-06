031e6f5d241ae0ec232360b699045f0b138b9141
Saharat.i <Saharat.Iamjaeng@gmail.com>
2025-06-06 14:10:34 +0700

    Create run_settings.sh

---
+++ run_settings.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+FOLDERS=("software" "manga" "ebook-store","data")
+
+# Loop สร้างโฟลเดอร์ทั้งหมด
+for FOLDER in "${FOLDERS[@]}"; do
+  if [ ! -d "$FOLDER" ]; then
+    echo "Creating folder: $FOLDER"
+    mkdir -p "$FOLDER"
+  else
+    echo "Folder already exists: $FOLDER"
+  fi
+done
